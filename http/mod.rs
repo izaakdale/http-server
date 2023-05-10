1024c9c04f0ffd2f32ab94d9ed654dc7ce1a0a26
Izaak Dale <izaakdaledev@gmail.com>
2023-05-10 22:01:09 +0100

    removing accidentals

--- http/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod request;
-pub use request::Request;
-pub use request::ParseError;
-
-pub mod method;
-pub use method::Method;
--- main.rs
+++
@@ -1,9 +0,0 @@
-use server::Server;
-
-mod http;
-mod server;
-
-fn main() {
-    let server = Server::new("localhost:9080".to_string());
-    server.run();
-}
